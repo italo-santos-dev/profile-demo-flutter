a8c35fb39720ce527e48577aeedad2127cf18fa7
dev-santos <italotayllor@gmail.com>
2021-05-27 14:00:45 -0300

    profile-demo-add-images

--- lib/core/app_images.dart
+++ lib/core/app_images.dart
@@ -1,7 +1,7 @@
 class AppImages {
   static String get logo => "assets/images/profile_logo.png";
   static String get meio => "assets/images/profile_meio.png";
-  static String get dev_quiz => "assets/images/profile_dev_quiz.png";
-  static String get logo_bethehero => "assets/images/profile_logo_bethehero.png";
-  static String get logo_flexplay => "https://raw.githubusercontent.com/dev-italosantos/website-flexplay-project/master/Imagens/PicsArt_12-11-09.34.48.png";
+  static String get devquiz => "assets/images/profile_dev_quiz.png";
+  static String get logobethehero => "assets/images/profile_logo_bethehero.png";
+  static String get logoflexplay => "assets/images/profile_logo_flexplay.png";
 }
