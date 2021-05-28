bdc71ab8e3dab3ac4f6e9df378c478edfd2e2518
dev-santos <italotayllor@gmail.com>
2021-05-27 21:34:28 -0300

    profile-demo-add-favorite

--- lib/components/widget/projects_component_widget.dart
+++ lib/components/widget/projects_component_widget.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:share/share.dart';
+import 'package:english_words/english_words.dart';
 
 import 'package:profile/models/project_model.dart';
 
 // ignore: avoid_web_libraries_in_flutter
 //import 'dart:html' as html;
 
-class ProjecComponentWidget extends StatelessWidget {
+class ProjecComponentWidget extends StatefulWidget {
   final Project _project;
   final double _bottomPadding;
   ProjecComponentWidget(this._project, this._bottomPadding, {size});
 
   @override
+  _ProjecComponentWidgetState createState() => _ProjecComponentWidgetState();
+}
+
+class _ProjecComponentWidgetState extends State<ProjecComponentWidget> {
+  final _saved = Set<WordPair>();
+  @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
     return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, _bottomPadding),
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, widget._bottomPadding),
       child: InkWell(
         onTap: () {},
         child: Padding(
@@ -26,12 +33,13 @@ class ProjecComponentWidget extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
               Expanded(
-                  flex: 40,
-                  child: Image.asset(
-                    _project.image,
-                    width: width * .25,
-                    height: width * .25,
-                  )),
+                flex: 40,
+                child: Image.asset(
+                  widget._project.image,
+                  width: width * .25,
+                  height: width * .25,
+                ),
+              ),
               Expanded(
                 flex: 3,
                 child: Container(),
@@ -44,31 +52,35 @@ class ProjecComponentWidget extends StatelessWidget {
                     direction: Axis.horizontal,
                     children: <Widget>[
                       Text(
-                        _project.name,
+                        widget._project.name,
                         style: Theme.of(context).textTheme.headline6,
                       ),
                       SizedBox(
                         height: height * .04,
                       ),
                       Text(
-                        _project.description,
+                        widget._project.description,
                         textScaleFactor: 1.2,
                         style: Theme.of(context).textTheme.caption,
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: <Widget>[
-                          IconButton(
-                            icon: Icon(Icons.favorite),
-                            onPressed: () {},
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.share),
-                            onPressed: () {
-                              Share.share(
-                                "Please Visit https://github.com/dev-italosantos",
-                              );
-                            },
+                          // IconButton(
+                          //   icon: Icon(Icons.favorite),
+                          //   onPressed: () {},
+                          // ),
+                          // IconButton(
+                          //   icon: Icon(Icons.share),
+                          //   onPressed: () {
+                          //     Share.share(
+                          //       "Please Visit https://github.com/dev-italosantos",
+                          //     );
+                          //   },
+                          // ),
+                          Expanded(
+                            flex: 3,
+                            child: _buildRow(_saved),
                           ),
                         ],
                       ),
@@ -83,8 +95,35 @@ class ProjecComponentWidget extends StatelessWidget {
     );
   }
 
-  // void onProjectClick() {
-  //   // ignore: unnecessary_null_comparison
-  //   if (_project.link != null) html.window.open(_project.link, 'adityadroid');
-  // }
+  Widget _buildRow(pair) {
+    final alreadySaved = _saved.contains(pair);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+          ),
+          onPressed: () {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(pair);
+              } else {
+                _saved.add(pair);
+              }
+            });
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.share),
+          onPressed: () {
+            Share.share(
+              "Please Visit https://github.com/dev-italosantos",
+            );
+          },
+        ),
+      ],
+    );
+  }
 }
