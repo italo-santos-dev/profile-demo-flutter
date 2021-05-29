24342cec695f4a4e2446bd851af50ddd1dca6856
dev-santos <italotayllor@gmail.com>
2021-05-29 19:09:21 -0300

    profile-demo-add-new-gradient

--- lib/core/app_gradients.dart
+++ lib/core/app_gradients.dart
@@ -1,15 +1,14 @@
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
 
 class AppGradients {
-  static final linear = LinearGradient(
-    colors: [
-      Color(0xFFDF7494),
-      Color(0xFF963772),
-      Color(0xFF592256),
-      Color(0xFF2D0C40),
-    ],
-    begin: Alignment.topRight,
-    end: Alignment.bottomLeft,
-    stops: [0.2, 0.5, 0.8, 0.7],
-  );
+  static final linear = LinearGradient(colors: [
+    Color(0xFFDF7494),
+    Color(0xFF963772),
+    Color(0xFF592256),
+    Color(0xFF2D0C40),
+  ], stops: [
+    0.0,
+    0.695
+  ], transform: GradientRotation(2.13959913 * pi));
 }
