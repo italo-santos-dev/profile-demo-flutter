f02ba7d6dfc3949104a8c65d55ffe741c6195c25
dev-santos <italotayllor@gmail.com>
2021-05-30 16:30:31 -0300

    profile-demo-modified-app-colors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -3,10 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   //coresPaleta
   static final Color lightcoral = Color(0xFFD9799C);
-  static final Color maroon = Color(0xFF592256);
   static final Color purple = Color(0xFF240940);
   static final Color white = Color(0xFFFFFFFF);
-
+  static final Color grey = Color(0xFF85819C);
   //levelButton
   static final Color buttonObservers = Color(0xFF592256);
 }
