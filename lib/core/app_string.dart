b7c3592c104cc49a9e5228864e62a496b14bbadb
dev-santos <italotayllor@gmail.com>
2021-05-30 14:30:04 -0300

    profile-demo-new-file-app-string

---
+++ lib/core/app_string.dart
@@ -0,0 +1,16 @@
+class Strings {
+  static const String about_me = 'About Me';
+  static const String about = 'About';
+  static const String me = 'Me';
+  static const String portfoli = 'Portfolio';
+  static const String portfolio = "Portfolio Italo Santos";
+  static const String o = 'o';
+  static const String headline =
+      'I\'am Italo Santos, Mobile App Developer from Lorem, Lorem';
+  static const String summary =
+      'Focused professional having excellent technical and communication skills, and offering 3 years of experience in Computer industry. Proficient at designing and formulating test automation frameworks, writing code in various languages, feature development and implementation. Specialize in thinking outside the box to find unique solutions to difficult engineering problems.';
+  static const String experience = 'Experience';
+  static const String skills_i_have = 'What Skill I Have';
+  static const String rights_reserved =
+      '© 2021 ITALO SANTOS. ALL RIGHTS RESERVED.';
+}
