89f43f99ce94ffa58a1584d37a4116b6686444ed
dev-santos <italotayllor@gmail.com>
2021-05-29 23:49:39 -0300

    profile-demo-new-img

--- lib/core/app_images.dart
+++ lib/core/app_images.dart
@@ -4,4 +4,5 @@ class AppImages {
   static String get devquiz => "assets/images/profile_dev_quiz.png";
   static String get logobethehero => "assets/images/profile_logo_bethehero.png";
   static String get logoflexplay => "assets/images/profile_logo_flexplay.png";
+  static String get profilei => "assets/images/profile_i.jpg";
 }
