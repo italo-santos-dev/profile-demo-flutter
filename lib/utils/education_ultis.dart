357a6ebe74394d44313389517461edb3e6bb1f80
dev-santos <italotayllor@gmail.com>
2021-05-30 15:30:45 -0300

    profile-demo-add-new-file-education

---
+++ lib/utils/education_ultis.dart
@@ -0,0 +1,23 @@
+// Education Methods:---------------------------------------------------------
+import 'package:profile/models/education.dart';
+
+final educationList = [
+  Education(
+    'Apr 2021',
+    'Present',
+    'Laborum ut esse sit fugiat tempor velit consequat duis.',
+    'Software Engineer',
+  ),
+  Education(
+    'Apr 2020',
+    'Apr 2020',
+    'Laborum ut esse sit fugiat tempor velit consequat duis.',
+    'Software Engineer',
+  ),
+  Education(
+    'July 2019',
+    'March 2019',
+    'Laborum ut esse sit fugiat tempor velit consequat duis.',
+    'Software Engineer',
+  ),
+];
