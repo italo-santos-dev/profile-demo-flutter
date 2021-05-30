bdadf406cc0d6d2ddb2edb1a37de331c4959e38a
dev-santos <italotayllor@gmail.com>
2021-05-29 22:52:34 -0300

    profile-demo-add-new-splash

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -5,7 +5,7 @@ import 'package:profile/home/home_page.dart';
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 3)).then(
+    Future.delayed(Duration(seconds: 7)).then(
       (value) => Navigator.pushReplacement(
         context,
         MaterialPageRoute(
@@ -14,13 +14,67 @@ class SplashPage extends StatelessWidget {
       ),
     );
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: AppGradients.linear,
-        ),
-        child: Center(
-          child: Image.asset(AppImages.logo),
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: AppGradients.linear,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: AppColors.white,
+                        radius: 50.0,
+                        child: Center(
+                          child: Image.asset(AppImages.logo),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 20.0),
+                      ),
+                      Text(
+                        "Portfolio Italo Santos",
+                        style: TextStyle(
+                          color: AppColors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),      
+              	SizedBox(
+                 height: 50,
+                 ),
+                    Text(
+                      "Lorem esse aliqua.",
+                      style: TextStyle(
+                          color: AppColors.white,
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
