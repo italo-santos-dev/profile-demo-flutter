9b07345823e0d4f6e56a77491a54e0734f54b8fd
dev-santos <italotayllor@gmail.com>
2021-05-30 14:26:52 -0300

    profile-demo-remove-color

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   //coresPaleta
   static final Color lightcoral = Color(0xFFD9799C);
-  static final Color violetred = Color(0xFFBF5486);
   static final Color maroon = Color(0xFF592256);
   static final Color purple = Color(0xFF240940);
   static final Color white = Color(0xFFFFFFFF);
