3915e98f9c1900ab55c709e931e3ffdd71315227
dev-santos <italotayllor@gmail.com>
2021-06-04 21:45:04 -0300

    profile-repository-modified-img-logo-busca-cep

--- lib/core/app_images.dart
+++ lib/core/app_images.dart
@@ -6,4 +6,6 @@ class AppImages {
   static String get logoflexplay => "assets/images/profile_logo_flexplay.png";
   static String get profilei => "assets/images/profile_i.jpg";
   static String get profileabout => "assets/images/profile_about.png";
+  static String get logoconsultacep => "assets/images/profile_logo_busca_cep.png";
+  static String get logoconversormedida => "assets/images/profile_converso_medida.png";
 }
