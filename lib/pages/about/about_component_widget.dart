3f982af3c46c3dfc010d3f16c9913db6d1784a0b
Ítalo Santos <italotayllor@gmail.com>
2021-06-09 23:37:43 -0300

    profile-demo-about-component-refactoring

--- lib/pages/about/about_component_widget.dart
+++ lib/pages/about/about_component_widget.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:profile_demo/core/app_colors.dart';
 import 'package:profile_demo/core/app_default.dart';
+import 'package:profile_demo/pages/about/widget/medium_about_component_widget.dart';
+import 'package:profile_demo/pages/about/widget/small_about_component_widget.dart';
 import 'package:profile_demo/utils/responsive_layout_ultis.dart';
 
 import 'widget/large_about_components_widget.dart';
-import 'widget/sections/content_about_widget.dart';
 
 class AboutComponentWidget extends StatelessWidget {
   @override
@@ -38,42 +39,9 @@ Widget buildBody(BuildContext context, BoxConstraints constraints) {
           minWidth: constraints.maxWidth, minHeight: constraints.maxHeight),
       child: ResponsiveLayoutUltis(
         largeScreen: LargeAboutComponentsWidget(),
-        mediumScreen: _buildMediumScreen(context),
-        smallScreen: _buildSmallScreen(context),
+        mediumScreen: MediumAboutComponentWidget(),
+        smallScreen: SmallAboutComponentWidget(),
       ),
-    ),
-  );
-}
-
-Widget _buildMediumScreen(BuildContext context) {
-  return IntrinsicHeight(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Expanded(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Expanded(flex: 1, child: ContentAboutWidget()),
-            ],
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-Widget _buildSmallScreen(BuildContext context) {
-  return IntrinsicHeight(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        Expanded(flex: 1, child: ContentAboutWidget()),
-        Divider(),
-        SizedBox(
-            height: ResponsiveLayoutUltis.isSmallScreen(context) ? 12.0 : 0.0),
-      ],
     ),
   );
 }
