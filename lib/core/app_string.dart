26f284cc598f148f8a2304d6bb99bc4a4ebd8d9f
Ítalo Santos <italotayllor@gmail.com>
2021-07-15 19:44:10 -0300

    profile-dev-italo-santos-refactorie

--- lib/core/app_string.dart
+++ lib/core/app_string.dart
@@ -1,16 +1,13 @@
 class Strings {
-  static const String about_me = 'About Me';
-  static const String about = 'About';
-  static const String me = 'Me';
-  static const String portfoli = 'Portfolio';
+  static const String about = 'Sobre';
+  static const String me = 'Mim';
   static const String portfolio = "Portfolio Italo Santos";
-  static const String o = 'o';
   static const String headline =
-      'I\'am Italo Santos, Software Developer App, Web and Descktop';
+      'Ítalo Santos, desenvolvedor de aplicativos, Web, Desktop e Mobile';
+  static const String experience = 'Experiência';
   static const String summary =
-      'Focused professional having excellent technical and communication skills, and offering 3 years of experience in Computer industry. Proficient at designing and formulating test automation frameworks, writing code in various languages, feature development and implementation. Specialize in thinking outside the box to find unique solutions to difficult engineering problems.';
-  static const String experience = 'Experience';
-  static const String skills_i_have = 'What Skill I Have';
+      'Técnico em informática encantado por tecnologias de desenvolvimento que atualmente estuda Flutter, CSharp e Node JS!';
+  static const String skills_i_have = 'Linguagens e ferramentas';
   static const String rights_reserved =
-      '© 2021 ITALO SANTOS. ALL RIGHTS RESERVED.';
+      '© 2021 ITALO SANTOS, TODOS OS DIREITOS RESERVADOS.';
 }
