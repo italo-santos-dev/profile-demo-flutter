97ac6df0380fed8e1925fdf65cc637dbdd0658e5
Ítalo Santos <italotayllor@gmail.com>
2021-07-16 16:03:03 -0300

    portfolio-demo-dev-italo-santos-refactoring-code-to-be-responsive

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -26,7 +26,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Container(
         child: tabWidgets.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
