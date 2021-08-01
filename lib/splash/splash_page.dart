dae34c32d01c96ea4acb82b6b40723cd386e3ace
Ítalo Santos <italotayllor@gmail.com>
2021-08-01 17:16:58 -0300

    profile-demo-updating-portfolio-projects

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:profile_demo/core/app_colors.dart';
 import 'package:profile_demo/core/app_gradients.dart';
 import 'package:profile_demo/core/app_images.dart';
