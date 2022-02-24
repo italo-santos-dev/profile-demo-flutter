2f609ea0d9013786e326dac83706518ca2f16c5b
Ítalo Santos <italotayllor@gmail.com>
2022-02-24 13:40:18 -0300

    feat: Adicionar IconButton arrow classe project

--- lib/pages/project/widget/sections/project_section_widget.dart
+++ lib/pages/project/widget/sections/project_section_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:profile_demo/core/app_default.dart';
 import 'package:profile_demo/core/core.dart';
+import 'package:profile_demo/screens/home/home_screen.dart';
 import 'package:profile_demo/tabs/projects_tab.dart';
 
 class ProjectSectionWidget extends StatelessWidget {
@@ -8,23 +9,40 @@ class ProjectSectionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      constraints: BoxConstraints(maxHeight: 900.0, minHeight: 700.0),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: AppColors.linda,
-      ),
-      child: Container(
-        margin: EdgeInsets.only(top: kDefaultPadding),
-        child: Stack(
-          children: [
-            LayoutBuilder(
-              builder: (context, constraints) {
-                return ProjectsTab();
-              },
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: AppColors.stefany,
             ),
-          ],
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(),
+                ),
+              );
+            }),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        constraints: BoxConstraints(maxHeight: 900.0, minHeight: 700.0),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.linda,
+        ),
+        child: Container(
+          margin: EdgeInsets.only(top: kDefaultPadding),
+          child: Stack(
+            children: [
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  return ProjectsTab();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
