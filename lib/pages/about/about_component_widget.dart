9e5554069659e613ad1135bea19bd8826b2209c1
Ítalo Santos <italotayllor@gmail.com>
2022-02-24 14:04:42 -0300

    feat: Adicionar IconButton arrow classe about

--- lib/pages/about/about_component_widget.dart
+++ lib/pages/about/about_component_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:profile_demo/core/app_colors.dart';
 import 'package:profile_demo/core/app_default.dart';
+import 'package:profile_demo/screens/home/home_screen.dart';
 
 import 'package:profile_demo/utils/responsive_layout_ultis.dart';
 
@@ -11,23 +12,40 @@ import 'widget/responsive/small_about_component_widget.dart';
 class AboutComponentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      constraints: BoxConstraints(maxHeight: 900.0, minHeight: 700.0),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: AppColors.purple,
-      ),
-      child: Container(
-        margin: EdgeInsets.only(top: kDefaultPadding),
-        child: Stack(
-          children: [
-            LayoutBuilder(
-              builder: (context, constraints) {
-                return buildBody(context, constraints);
-              },
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: AppColors.stefany,
             ),
-          ],
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomeScreen(),
+                ),
+              );
+            }),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        constraints: BoxConstraints(maxHeight: 900.0, minHeight: 700.0),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: AppColors.purple,
+        ),
+        child: Container(
+          margin: EdgeInsets.only(top: kDefaultPadding),
+          child: Stack(
+            children: [
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  return buildBody(context, constraints);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
