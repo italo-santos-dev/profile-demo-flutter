ce5bde2766c325c51c3994bdc2aa6a718535381c
Ítalo Santos <italotayllor@gmail.com>
2022-02-28 00:03:46 -0300

    feat: Adicionar hiperlink de redirecionamento para redes sociais e email. closes #22

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -11,11 +11,10 @@ import 'package:profile_demo/screens/main/components/area_info_text.dart';
 import 'package:profile_demo/screens/main/components/knowledges.dart';
 import 'package:profile_demo/screens/main/components/my_info.dart';
 import 'package:profile_demo/screens/main/components/skills.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SideMenu extends StatelessWidget {
-  const SideMenu({
-    Key? key
-  }) : super(key: key);
+  const SideMenu({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,90 +24,108 @@ class SideMenu extends StatelessWidget {
           MyInfo(),
           Expanded(
               child: SingleChildScrollView(
-                padding: EdgeInsets.all(defaultPadding),
-                child: Column(
-                  children: [
-                    AreaInfoText(
-                      title: "Residência",
-                      text: "Brasil",
-                    ),
-                    AreaInfoText(
-                      title: "Cidade",
-                      text: "Fortaleza",
-                    ),
-                    AreaInfoText(
-                      title: "Era",
-                      text: "18",
-                    ),
-                    Skills(),
-                    SizedBox(height: defaultPadding),
-                    Knowledges(),
-                    Divider(),
-                    SizedBox(height: defaultPadding / 2),
-                    TextButton(
-                        onPressed: () {
-                          openFile(
-                            url: 'https://drive.google.com/file/d/1gY05JoeEtzmDWGdvsnfZKzlDj-D3hOsj/view?usp=sharing',
-                            fileName: 'CurriculoItaloSantos.pdf',
-                          );
-                        },
-                        child: FittedBox(
-                          child: Row(
-                            children: [
-                              Text(
-                                  "DOWNLOAD CV",
-                                style: TextStyle(
-                                  color: Theme.of(context).textTheme.bodyText1!.color,
-                                )
-                              ),
-                              SizedBox(width: defaultPadding / 2),
-                              SvgPicture.asset(
-                                   AppIcons.downloadIcon,
-                                width: 16.0,
-                                height: 16.0,
-                              )
-                            ],
-                          ),
-                        ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(top: defaultPadding / 2),
-                      color: Color(0xFF24242E),
-                      child: Row(
-                        children: [
-                          Spacer(),
-                          IconButton(
-                            onPressed: () {},
-                            icon: SvgPicture.asset(
-                              AppIcons.linkedinIcon,
-                              width: 18.0,
-                              height: 18.0,
-                            )
-                          ),
-                          IconButton(
-                              onPressed: () {},
-                              icon: SvgPicture.asset(AppIcons.githubIcon)
-                          ),
-                          IconButton(
-                              onPressed: () {},
-                              icon: SvgPicture.asset(
-                                AppIcons.emailIcon,
-                                width: 20.0,
-                                height: 20.0,
-                              ),
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                    )
-                  ],
+            padding: EdgeInsets.all(defaultPadding),
+            child: Column(
+              children: [
+                AreaInfoText(
+                  title: "Residência",
+                  text: "Brasil",
                 ),
-              )
-          )
+                AreaInfoText(
+                  title: "Cidade",
+                  text: "Fortaleza",
+                ),
+                AreaInfoText(
+                  title: "Era",
+                  text: "18",
+                ),
+                Skills(),
+                SizedBox(height: defaultPadding),
+                Knowledges(),
+                Divider(),
+                SizedBox(height: defaultPadding / 2),
+                TextButton(
+                  onPressed: () {
+                    openFile(
+                      url:
+                          'https://drive.google.com/file/d/1gY05JoeEtzmDWGdvsnfZKzlDj-D3hOsj/view?usp=sharing',
+                      fileName: 'CurriculoItaloSantos.pdf',
+                    );
+                  },
+                  child: FittedBox(
+                    child: Row(
+                      children: [
+                        Text("DOWNLOAD CV",
+                            style: TextStyle(
+                              color:
+                                  Theme.of(context).textTheme.bodyText1!.color,
+                            )),
+                        SizedBox(width: defaultPadding / 2),
+                        SvgPicture.asset(
+                          AppIcons.downloadIcon,
+                          width: 16.0,
+                          height: 16.0,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: defaultPadding / 2),
+                  color: Color(0xFF24242E),
+                  child: Row(
+                    children: [
+                      Spacer(),
+                      IconButton(
+                          onPressed: () {
+                            launch(
+                                'https://www.linkedin.com/in/italo-santos-dev/');
+                          },
+                          icon: SvgPicture.asset(
+                            AppIcons.linkedinIcon,
+                            width: 18.0,
+                            height: 18.0,
+                          )),
+                      IconButton(
+                          onPressed: () {
+                            launch('https://github.com/dev-italosantos');
+                          },
+                          icon: SvgPicture.asset(AppIcons.githubIcon)),
+                      IconButton(
+                        onPressed: () async {
+                          final Uri params = Uri(
+                            scheme: 'mailto',
+                            path: 'isantos.worldtec@gmail.com',
+                            query:
+                                'subject=Reportar&body=Olá [Primeiro nome], \n\nObrigado por entrar em contato. Eu sinto muito! Entendo completamente sua frustração com [o problema.]  \nAbri um ticket para o time responsável e nossa equipe está priorizando seu problema e fará todo o possível para corrigi-lo o mais rápido possível. Entrarei em contato com você assim que tiver mais informações. \n\nAtenciosamente, [Nome]',
+                          );
+
+                          String url = params.toString();
+
+                          if (await canLaunch(url)) {
+                            await launch(url);
+                          } else {
+                            print('Could not launch $url');
+                          }
+                        },
+                        icon: SvgPicture.asset(
+                          AppIcons.emailIcon,
+                          width: 20.0,
+                          height: 20.0,
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ))
         ],
       ),
     );
   }
+
   Future openFile({required String url, required String? fileName}) async {
     final file = await downloadFile(url, fileName!);
 
@@ -116,31 +133,26 @@ class SideMenu extends StatelessWidget {
 
     print('Path: ${file.path}');
 
-
     OpenFile.open(file.path);
-
   }
 
   Future<File?> downloadFile(String url, String name) async {
     final appStorage = await getApplicationDocumentsDirectory();
     final file = File('${appStorage.path}/$name');
 
-   try {
-     final response = await Dio().get(
-         url,
-         options: Options(
-           responseType: ResponseType.bytes,
-           followRedirects: false,
-           receiveTimeout: 0,
-         )
-     );
+    try {
+      final response = await Dio().get(url,
+          options: Options(
+            responseType: ResponseType.bytes,
+            followRedirects: false,
+            receiveTimeout: 0,
+          ));
 
-     final raf = file.openSync(mode: FileMode.write);
-     raf.writeFromSync(response.data);
-     return file;
-
-   } catch (err) {
-     return null;
-   }
+      final raf = file.openSync(mode: FileMode.write);
+      raf.writeFromSync(response.data);
+      return file;
+    } catch (err) {
+      return null;
+    }
   }
 }
