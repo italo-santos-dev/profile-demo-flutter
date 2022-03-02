2b1bd305ae372f21ec50dbb30357b6f573b12467
dev-italosantos <italotayllor@gmail.com>
2022-03-02 10:00:45 -0300

    feat: Adicionar about

--- lib/core/app_string.dart
+++ lib/core/app_string.dart
@@ -9,7 +9,7 @@ class Strings {
   static const String portfolio = 'Portfolio Italo Santos';
   static const String headline = 'Prazer, sou técnico de informática encantado com tecnologias de desenvolvimento.';
   static const String experience = 'Experiência';
-  static const String summary = 'Atualmente estudo Kotlin uma linguagem de programação multiplataforma, orientada a objetos e funcional, concisa e estaticamente tipada, além disso, Flutter um kit de desenvolvimento de interface de usuário, de código aberto, criado pelo Google, que possibilita a criação de aplicativos compilados nativamente. No momento atual pode compilar para Android, iOS, Windows, Mac, Linux, Google Fuchsia e Web. Incremento em função do usuário, pensando em maneiras que tornem sua experiência mais fácil e rápido, mantendo padrões baseados em boas práticas, priorizando códigos limpos e organizados.';
+  static const String summary = 'Atualmente estudo Java encaminhado para Kotlin uma linguagem de programação multiplataforma, orientada a objetos e funcional, concisa e estaticamente tipada, além disso, Flutter um kit de desenvolvimento de interface de usuário, de código aberto, criado pelo Google, que possibilita a criação de aplicativos compilados nativamente, ou seja, "asmo" desenvolvimento Android. Incremento em função do usuário, pensando em maneiras que tornem sua experiência mais fácil e rápido, mantendo padrões baseados em boas práticas, priorizando códigos limpos e organizados.';
   static const String skills_i_have = 'Linguagens e ferramentas';
   static const String contact_i_have = 'Contatos';
   static const String rights_reserved = '© 2022 ITALO SANTOS, TODOS OS DIREITOS RESERVADOS.';
